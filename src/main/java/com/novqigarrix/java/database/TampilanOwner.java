3ed4869e597ffc171207bafc1da7fc1d809b93fc
NovqiGarrix <novqigarrix@gmail.com>
2023-01-19 10:30:37 +0700

    Membuat Tampilan owner

---
+++ src/main/java/com/novqigarrix/java/database/TampilanOwner.java
@@ -0,0 +1,71 @@
+package com.novqigarrix.java.database;
+
+import com.novqigarrix.java.database.model.ProductTransactionModel;
+import com.novqigarrix.java.database.repository.TransactionRepositoryImpl;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.SQLException;
+
+public class TampilanOwner extends JFrame {
+
+    // TODO: UnComment this when merging the code
+    static {
+        System.setProperty("JDBC_URL", "jdbc:mysql://localhost:3306/tubes_pemograman2_test");
+        System.setProperty("MYSQL_USERNAME", "root");
+        System.setProperty("MYSQL_PASSWORD", "mysql");
+    }
+
+    private JPanel mainPanel;
+    private JTable mainTable;
+
+    TampilanOwner() {
+        super("Tampilan Owner");
+
+        this.setContentPane(mainPanel);
+        this.setSize(600, 400);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame  = new TampilanOwner();
+
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.addColumn("ID PRODUK");
+        model.addColumn("NAMA PRODUK");
+        model.addColumn("TERJUAL");
+        model.addColumn("STOCK");
+        model.addColumn("HARGA BELI");
+        model.addColumn("HARGA JUAL");
+
+        TransactionRepositoryImpl repo = new TransactionRepositoryImpl();
+
+        try {
+
+            ProductTransactionModel[] dataTransaksi = repo.findAllProductAndTransactions();
+
+            for (ProductTransactionModel data : dataTransaksi) {
+                Object[] row = {
+                        data.getIdProduk(),
+                        data.getNamaProduk(),
+                        data.getTerjual(),
+                        data.getStok(),
+                        "$" + data.getHargaBeli(),
+                        "$" + data.getHarga()
+                };
+                model.addRow(row);
+            }
+
+            mainTable = new JTable(model);
+
+        } catch (SQLException e) {
+            System.out.println("Terjadi error saat mengambil data transaksi di database!");
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
